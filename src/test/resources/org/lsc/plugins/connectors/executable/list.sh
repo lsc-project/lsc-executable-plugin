af28120af195da0ec026b6546a436f2443068aa5
Raphaël OUAZANA <raphael.ouazana@gmail.com>
2014-02-21 10:51:18 +0000

    Fix clean phase for ExecutableLdifDestinationService - workaround for https://issues.apache.org/jira/browse/DIRAPI-175 (references #446)

--- src/test/resources/org/lsc/plugins/connectors/executable/list.sh
+++ src/test/resources/org/lsc/plugins/connectors/executable/list.sh
@@ -2,7 +2,7 @@
 
 echo "Scanning repository content ..." 1>&2
 
-ldapsearch -x -LLL -H "$LDAP_URL" -D "$LDAP_BIND_DN" -w "$LDAP_BIND_PW" -b "$LDAP_BASE" -s "$LDAP_SCOPE" uid \
+ldapsearch -x -LLL -H "$LDAP_URL" -D "$LDAP_BIND_DN" -w "$LDAP_BIND_PW" -b "$LDAP_BASE" -s "$LDAP_SCOPE" '(objectClass=person)' objectClass \
 	| sed -e "s/^\(dn: .*\),dc=lsc-project, *dc=org$/\1/g" 
 
 exit $?
